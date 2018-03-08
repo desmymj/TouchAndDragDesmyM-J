c661cdc3afb78c3d1a6cb8743b2e694dfb6a3260
Desmy MJ <desmy.michaelis-joseph@ocsbstudent.ca>
2018-03-08 09:50:20 -0500

    added code

--- main.lua
+++ main.lua
@@ -1,7 +1,13 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: TouchAndDrag
+-- Name: Desmy M-J
+-- Course: ICS2O/3C
+-- This program displays images tthat can be dragged
+--around on the screen
 
--- Your code here
+--hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--local Variables. I am still trying to get the x-value to be set properly
+local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
+local yellowGirl = display.newImageRect("Images/yellowGirl.png", 150, 150)
+local yellowGirlWidth = yellowGirl.Width
